def3d8bd67379cd27b707fa7f7972aa48aa39dc7
13923 <Christopher.alessio@vtsd.com>
2025-05-28 12:33:26 -0400

    first push

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Create class HelloWorld
+public class HelloWorld {
+
+    // Create main method
+    public static void main(String[] args) {
+        // Output to the window hello world
+        System.out.println("Hello World");
+    }
+
+}
---
+++ Introduction/VariableIntro.java
@@ -0,0 +1,22 @@
+package Introduction;
+
+public class VariableIntro {
+
+    public static void main(String[] args) {
+
+        /*
+         *To Create a variable:
+         * datatype = name
+         *
+         */
+        double salary = 10000.56;
+        int bonus = 250;
+
+        bonus = (int) salary;
+
+        System.out.println(salary);
+        System.out.println(bonus);
+    }
+
+
+}
